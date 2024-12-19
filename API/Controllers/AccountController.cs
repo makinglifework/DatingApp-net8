9343e3de7793916950cb586187cbe4d036dabfa0
devops-manna <devops-manna@gmail.com>
2024-12-20 00:54:45 +0800

    End of Section 4

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,67 @@
+using System.Security.Cryptography;
+using System.Text;
+using API.Controllers.Entities;
+using API.Data;
+using API.DTOs;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
+{
+    [HttpPost("register")] // account/register
+    public async Task<ActionResult<UserDTO>> Register(RegisterDTO registerDto)
+    {
+        if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+            UserName = registerDto.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return new UserDTO
+        {
+            Username = user.UserName,
+            Token = tokenService.CreateToken(user)
+        };
+    }
+
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
+    {
+        var user = await context.Users.FirstOrDefaultAsync( x => 
+            x.UserName == loginDTO.Username.ToLower());
+
+        if (user == null) return Unauthorized("Invalid username ");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+        }
+        
+        return new UserDTO
+        {
+            Username = user.UserName,
+            Token = tokenService.CreateToken(user)
+        };
+    }
+    
+    private async Task<bool> UserExists(string username)
+    {
+        return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());   // Bob != bob
+    }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,7 +1,10 @@
-namespace API.Controllers.Entities;
-
-public class AppUser
+namespace API.Controllers.Entities
 {
-    public int Id { get; set; }
-    public required string  UserName { get; set; }
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public required string  UserName { get; set; }
+        public required byte[] PasswordHash { get; set; }
+        public required byte[] PasswordSalt { get; set; }
+    }
 }
